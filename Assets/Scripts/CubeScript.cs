7505462b6da16322c27b30feb000233b25a20854
Will Faircloth <willfaircloth@icloud.com>
2022-12-29 02:08:15 -0500

    hotfix for gravity control box pt 2

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -5,10 +5,7 @@ using ElRaccoone.Tweens;
 
 public class CubeScript : MonoBehaviour
 {
-    int gravityScale = 1;
-
     public float jumpForce = 8;
-
     public float WORLD_MIN_X = -7.5f;
 	public float WORLD_MIN_Y = -5.5f;
 	public float WORLD_MAX_X =  20.0f;
@@ -21,6 +18,8 @@ public class CubeScript : MonoBehaviour
     // position for resetting
     Vector3 startPos;
 
+    Rigidbody2D cubeRigid;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,18 +31,21 @@ public class CubeScript : MonoBehaviour
     
         // setup startpos
         startPos = transform.position;
+
+        cubeRigid = GetComponent<Rigidbody2D>();
     }
+
     // Update is called once per frame
     void FixedUpdate() {
         // move the object forward or backwards according to the horizontal axis
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 5 * gravityScale,0,0));
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 5 * cubeRigid.gravityScale,0,0));
         spriteClamp.movementLimiter(transform.position, gameObject.transform);
 		cameraClamp.movementLimiter(transform.position, Camera.main.transform);
     }
 
     void Update() { 
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < 0.001f) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, gravityScale*jumpForce), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, cubeRigid.gravityScale*jumpForce), ForceMode2D.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
@@ -55,17 +57,16 @@ public class CubeScript : MonoBehaviour
     public void FlipGravity() {
         // do not rotate if flipping is in progress
         if (Camera.main.transform.rotation.z != 0 && Camera.main.transform.rotation.z != 1) return;
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
         float rotation = 0;
         if (Camera.main.transform.rotation.z == 0) 
         { 
             rotation = -180; 
-            rigidbody.gravityScale = -1;            
+            cubeRigid.gravityScale = -1;            
         } else if (Camera.main.transform.rotation.z == 1) {
-            rigidbody.gravityScale = 1;            
+            cubeRigid.gravityScale = 1;            
         }
-        rigidbody.velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        cubeRigid.velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
         Camera.main.TweenRotationZ(rotation, 0.25f);
     }
 
