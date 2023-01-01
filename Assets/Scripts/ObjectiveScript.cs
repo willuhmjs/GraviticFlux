9bc1115ee440af715eb9553f21efb6af11826723
William Faircloth <willuhmjs@gmail.com>
2022-12-31 21:51:51 -0500

    transitions

--- Assets/Scripts/ObjectiveScript.cs
+++ Assets/Scripts/ObjectiveScript.cs
@@ -9,27 +9,38 @@ public class ObjectiveScript : MonoBehaviour
     int currentScene;
     int numScenes;
 
+    GameObject transition;
+    CanvasGroup canvasGroup;
+
     // Start is called before the first frame update
     void Start()
     {
         currentScene = SceneManager.GetActiveScene().buildIndex;
         numScenes = SceneManager.sceneCountInBuildSettings;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        transition = GameObject.Find("Transition");
+        canvasGroup = transition.GetComponent<CanvasGroup>();
+
+        if (currentScene != 0) {
+            canvasGroup.alpha = 1;
+            canvasGroup.TweenCanvasGroupAlpha(0, 1f);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Player") {
-            gameObject.TweenSpriteRendererAlpha(0, 0.5f);
-            NextLevel();
+            gameObject.TweenSpriteRendererAlpha(0, 0.25f);
+            NextLevel(0.75f);
         }
     }
 
-    public void NextLevel() {
+    public void NextLevel(float transitionTime) {
+        StartCoroutine(NextLevelIEnumerator(transitionTime));
+    }
+
+    public IEnumerator NextLevelIEnumerator(float transitionTime) {
+        canvasGroup.TweenCanvasGroupAlpha(1, transitionTime);
+        yield return new WaitForSeconds(transitionTime);
         if (currentScene < numScenes - 1) {
             SceneManager.LoadScene("Level" + (currentScene + 1));
         } else {
