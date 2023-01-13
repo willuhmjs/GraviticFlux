f96ccbd4b7c2d475cae5285ac7e1671fb21d9593
William Faircloth <willuhmjs@gmail.com>
2023-01-13 15:05:16 -0500

    fix music bugs, delete prefab

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,15 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    public AudioClip clip;
-    public AudioSource source;
+    public AudioClip clipLevel0;
+    public AudioClip clipContinuing;
+    AudioSource source;
+    private static MusicPlayer instance = null;
+    private bool isContinuingMusic = false;
 
     private void Awake()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
         DontDestroyOnLoad(gameObject);
+        source = GetComponent<AudioSource>();
+        SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+    private void OnSceneChanged(Scene current, Scene next)
+    {
+        if (next.name == "Level0")
+        {
+            if(isContinuingMusic)
+            {
+                source.Stop();
+                isContinuingMusic = false;
+            }
+            source.clip = clipLevel0;
+            source.Play();
+        }
+        else
+        {
+            if(!isContinuingMusic)
+            {
+                source.clip = clipContinuing;
+                source.Play();
+                isContinuingMusic = true;
+            }
+        }
     }
 }
-
