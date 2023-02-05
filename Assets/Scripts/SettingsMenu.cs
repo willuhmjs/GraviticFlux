9f42be96e999d4e82e264b2bdd12ed9c25c4bca3
William Faircloth <willuhmjs@gmail.com>
2023-02-05 16:59:42 -0500

    merge settings menu scripts together

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class SettingsMenu : MonoBehaviour
 {
     private static SettingsMenu instance;
+    public Button[] tabs;
+    public Image[] panels;
+    private int activeTabIndex = 0;
 
     private void Awake()
     {
@@ -22,11 +24,21 @@ public class SettingsMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        SwitchTab(0);
         gameObject.SetActive(false);
     }
 
     public void ExitMenu() {
         gameObject.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    public void SwitchTab(int index)
+    {
+        tabs[activeTabIndex].interactable = true;
+        tabs[index].interactable = false;
+        panels[activeTabIndex].gameObject.SetActive(false);
+        panels[index].gameObject.SetActive(true);
+        activeTabIndex = index;
     }
 }
