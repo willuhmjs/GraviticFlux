c6d5870b7ca43df3c3a141ebf7d2b571f0a293da
William Faircloth <willuhmjs@gmail.com>
2023-02-16 16:36:41 -0500

    fix(settings): set latest level to 1 for start

--- Assets/Scripts/NoMono/SettingsData.cs
+++ Assets/Scripts/NoMono/SettingsData.cs
@@ -19,7 +19,7 @@ public enum Achievements {
 [System.Serializable]
 public class SettingsData
 {
-    public int latestLevel {get; set;} = 0;
+    public int latestLevel {get; set;} = 1;
 
     internal Dictionary<PlayerAction, KeyCode> controls {get; set;} = new Dictionary<PlayerAction, KeyCode>() {
         {PlayerAction.MoveLeft, KeyCode.A},
